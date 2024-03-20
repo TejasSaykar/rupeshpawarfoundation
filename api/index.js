f42f77126299d89f159cf74e79a709ab5f400780
TejasSaykar <tejassaykar2001@gmail.com>
2024-03-20 13:39:03 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -54,7 +54,7 @@ const httpsServer = https.createServer(credentials, app);
 
 
 
-const port = 8181;
+const port = 8282;
 app.listen(port, () => {
     console.log(`SERVER IS RUNNING ON http://localhost:${port}`);
 })
